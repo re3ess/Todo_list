a4a2e5eb7928817c74df4eda8725606846dfff73
DK <re4ess@gmail.com>
2023-01-02 12:17:23 +0900

    message2

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Header from './components/Header/Header';
+import TodoList from './components/TodoList/TodoList';
+import { DarkModeProvider } from './context/DarkModeContext';
+import './index.css';
+
+const filters = ['all', 'active', 'completed'];
+function App() {
+  const [filter, setFilter] = useState(filters[0]);
+
+  return (
+    <DarkModeProvider>
+      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
+      <TodoList filter={filter} />
+    </DarkModeProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
+import styles from './Todo.module.css';
+
+export default function Todo({ todo, onUpdate, onDelete }) {
+  const { id, text, status } = todo;
+  const handleChange = (e) => {
+    onUpdate({ ...todo, status: e.target.checked ? 'completed' : 'active' });
+  };
+  const handleDelete = () => onDelete(todo);
+  return (
+    <li className={styles.todo}>
+      <input
+        className={styles.checkbox}
+        type="checkbox"
+        id={id}
+        checked={status === 'completed'}
+        onChange={handleChange}
+      />
+      <lable htmlFor={id} className={styles.text}>
+        {text}
+      </lable>
+      <span className={styles.icon}>
+        <button onClick={handleDelete} className={styles.button}>
+          <FaTrashAlt />
+        </button>
+      </span>
+    </li>
+  );
+}
